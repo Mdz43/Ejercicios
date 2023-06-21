d61d003621559e2f2954d45cf3fe6962110a147d
Mdz43 <lautaromiranda@live.com.ar>
2023-06-21 00:27:50 -0300

    challenge5 hecho

---
+++ challenge5.js
@@ -0,0 +1,20 @@
+let numero1 = 5;
+let numero2 = 10;
+let galleta = "pepitos";
+
+console.log(numero1 + numero2);
+console.log(numero1 - numero2);
+console.log(numero1 * numero2);
+console.log(numero1 / numero2);
+
+if(numero1 % 2 === 0){
+    console.log(`el primer numero ${numero1} es par`)
+}else{
+    console.log(`el primer numero ${numero1} es impar`)
+}
+
+if(numero2 % 2 === 0){
+    console.log(`el segundo numero ${numero2} es par`)
+}else{
+    console.log(`el segundo numero ${numero2} es impar`)
+}
